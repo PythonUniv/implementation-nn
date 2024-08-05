2ae476a6f8e1607bc008dec6d83e8acf5eeed15c
Dima <126197030+PythonUniv@users.noreply.github.com>
2024-08-05 23:53:09 +0300

    Add files via upload

---
+++ clip/train.py
@@ -0,0 +1,9 @@
+from clip import DistilBertEncoder, VisionTransformerEncoder
+
+
+if __name__ == '__main__':
+    text_encoder = DistilBertEncoder()
+    vision_encoder = VisionTransformerEncoder()
+    print(text_encoder)
+    print()
+    print(vision_encoder.)   
