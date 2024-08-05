99facc6fb87ee8e42b4d19be660dfd40a2f34f8d
Dima <126197030+PythonUniv@users.noreply.github.com>
2024-08-05 23:53:33 +0300

    Delete clip/train.py

--- clip/train.py
+++
@@ -1,9 +0,0 @@
-from clip import DistilBertEncoder, VisionTransformerEncoder
-
-
-if __name__ == '__main__':
-    text_encoder = DistilBertEncoder()
-    vision_encoder = VisionTransformerEncoder()
-    print(text_encoder)
-    print()
-    print(vision_encoder.)   
