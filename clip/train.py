beaa1131d06349f2d1c0afdc84bc5c6fda8f2318
Dima <126197030+PythonUniv@users.noreply.github.com>
2024-08-06 18:30:00 +0300

    Add files via upload

--- clip/train.py
+++ clip/train.py
@@ -1,3 +1,5 @@
+import argparse
+
 from clip import DistilBertEncoder, VisionTransformerEncoder, CLIP, train
 from dataset import get_data_loaders
 
@@ -10,5 +12,12 @@ if __name__ == '__main__':
         distil_bert_encoder, vision_transformer_encoder,
         text_encoder_dim=768, vision_encoder_dim=768, proj_dim=256)
     
-    train_loader, val_loader = get_data_loaders(batch_size=128, num_workers=4)
-    train(clip, train_loader, val_loader, epochs=1)
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('-batch_size', default=128)
+    argument_parser.add_argument('-num_workers', default=0)
+    argument_parser.add_argument('-epochs', default=1)
+    arguments = argument_parser.parse_args()
+    
+    train_loader, val_loader = get_data_loaders(batch_size=arguments.batch_size, num_workers=arguments.num_workers)
+    
+    train(clip, train_loader, val_loader, epochs=arguments.epochs)
