29fdab936d0c11210fb247cdb46c92a4d8645cd5
Dima <126197030+PythonUniv@users.noreply.github.com>
2024-08-07 23:19:00 +0300

    Add files via upload

--- clip/train.py
+++ clip/train.py
@@ -12,8 +12,6 @@ if __name__ == '__main__':
         distil_bert_encoder, vision_transformer_encoder,
         text_encoder_dim=768, vision_encoder_dim=768, proj_dim=256)
     
-    clip.compile()
-    
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('-batch_size', default=128, type=int)
     argument_parser.add_argument('-num_workers', default=0, type=int)
