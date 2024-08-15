f7aa5813247f85b09f68cc2faea3f20ecf372aa4
Dima <126197030+PythonUniv@users.noreply.github.com>
2024-08-15 15:59:36 +0300

    Add files via upload

--- segformer/dataset.py
+++ segformer/dataset.py
@@ -32,9 +32,10 @@ class SegmentationDataset(Dataset):
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         if self.idx == self.current_parquet_rows:
             self.next_parquet()
-        
+
         image = np.array(Image.open(BytesIO(self.current_parquet.iloc[self.idx]['image']['bytes'])))
         ground_truth = np.array(Image.open(BytesIO(self.current_parquet.iloc[self.idx]['semantic_segmentation']['bytes'])))[:, :, 0]
+        self.idx += 1
         image, ground_truth = self.preprocessor(image, ground_truth, inference=(not self.augmentation), to_torch=True)
         return image, ground_truth
         
@@ -52,7 +53,7 @@ def get_data_loader(parquets: list[str], batch_size: int, height: int, width: in
     directory = dir or os.path.join(os.path.dirname(__file__), 'dataset', 'data')
     parquets = [os.path.join(directory, name) for name in parquets]
     dataset = SegmentationDataset(parquets, height, width, train)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=2)
     return loader
 
 
